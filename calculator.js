49bb6b34931cedee799cf60335d583a31e6db285
mbucataru <mbucatarugithub@gmail.com>
2023-08-04 12:06:48 -0400

    Add calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = "";
+let secondNum = "";
+let operator = "blank";
+const blank = "";
+const firstBlank = "blank"
 function add(a, b)
 {
     return a + b;
@@ -18,6 +20,8 @@ function multiply(a, b)
 
 function divide(a, b)
 {
+    if (b == 0)
+        return "Really? Refresh the page";
     return a / b;
 }
 
@@ -27,8 +31,78 @@ function operate(firstNum, secondNum, operator)
         return add(firstNum, secondNum);
     else if (operator == "-")
         return subtract(firstNum, secondNum);
-    else if (operator == "*")
+    else if (operator == "x")
         return multiply(firstNum, secondNum);
     else if (operator == "/")
         return divide(firstNum, secondNum);
 }
+
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display")
+display.textContent = "0";
+
+function populateOperation(e)
+{
+    if (firstNum != "Really? Refresh the page")
+    {
+        let input = e.target.textContent
+        if (!(isNaN(input)))
+        {
+            if (firstNum == blank || operator == firstBlank)
+            {
+                firstNum += input;
+            }
+            else 
+            {
+                secondNum += input;
+            }
+            if (operator == firstBlank)
+            {
+                display.textContent = `${firstNum}`;
+            }
+            else
+            {
+                display.textContent = `${secondNum}`;
+            }
+        }
+        else if (isNaN(input))
+        {
+            if (input == "C")
+            {
+                display.textContent = "0";
+                firstNum = blank;
+                secondNum = blank;
+                operator = firstBlank;
+            }
+            else if (input == "=")
+            {
+                if (firstNum != blank && secondNum != blank && operator != blank)
+                {
+                    firstNum = operate(+firstNum, +secondNum, operator);
+                    console.log(firstNum, secondNum, operator);
+                    display.textContent = `${firstNum}`;
+                    secondNum = blank;
+                    operator = blank;
+                }
+            }
+            // any operator
+            else
+            {
+                if (firstNum != blank && secondNum != blank && operator != blank)
+                {
+                    console.log("the thing i am expecting happens");
+                    console.log(firstNum, secondNum, operator);
+                    firstNum = operate(+firstNum, +secondNum, operator);
+                    console.log(firstNum, secondNum, operator);
+                    display.textContent = `${firstNum}`;
+                    secondNum = blank;
+                    operator = blank;
+                }
+                console.log("this is happening?")
+                operator = input;
+            }
+        } 
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', populateOperation));
